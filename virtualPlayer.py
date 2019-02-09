135135d4818ca5a8fc2c7579832c9be91b90adb9
kaito <39236776+kaito-cpro@users.noreply.github.com>
2019-02-09 23:55:39 +0900

    Update virtualPlayer.py

--- virtualPlayer.py
+++ virtualPlayer.py
@@ -4,3 +4,20 @@ class VirtualPlayer:
     def __init__(self):
         self.player1 = PlayerFactory().create_player(1)
         self.player2 = PlayerFactory().create_player(2)
+
+        self.player = None
+        self.set_former_player()
+
+        self.turn = {self.player1: self.player2,    \
+                     self.player2: self.player1}
+
+    def set_former_player(self):
+        ''' ランダムに先攻を決める '''
+        if random.randint(0, 1) == 0:
+            self.player = self.player1
+        else:
+            self.player = self.player2
+
+    def switch(self):
+        ''' 手番の交代 '''
+        self.player = self.turn[self.player]
