c7e51ef2bbad20f96f445d5a6710ed0d927d14d3
kaito <blue7-go0112@docomo.ne.jp>
2019-03-11 01:28:25 +0900

    test commit to branch kaito/master

---
+++ agentKaito.py
@@ -0,0 +1,29 @@
+from common import *
+from player import Player
+
+class Kaito(Player):
+    ''' Kaito の agent '''
+
+    def __init__(self, player_num):
+        self.player_num = player_num
+
+    def set_card(self):
+        pass
+
+    def select_guard(self):
+        pass
+
+    def select_attack(self):
+        pass
+
+    def shuffle(self):
+        pass
+
+    def call(self):
+        pass
+
+    def end_process(self, winner, cards_record):
+        pass
+
+    def get_log(self, log):
+        pass
