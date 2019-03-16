8547e65bbb8e6f936a3efbbddff9fe7ebb835eec
kaito <blue7-go0112@docomo.ne.jp>
2019-03-16 23:36:47 +0900

    improve playerFactory

--- playerFactory.py
+++ playerFactory.py
@@ -1,6 +1,7 @@
 from player import Player
 from testPlayers import *
 from myTestPlayers import *
+from agentKaito import *
 
 class PlayerFactory:
     ''' Player の生成クラス '''
@@ -9,10 +10,11 @@ class PlayerFactory:
         1: Random,
         2: Human,
         3: MyTestPlayer1}
-
+        
     def create_player(player_num):
         print(f'player{player_num}を選択してください')
-        print('  ', PlayerFactory.player_dict)
+        for num, player in PlayerFactory.player_dict.items():
+            print('  ', num, ':', player)
         selected_player = int(input((f'player{player_num}: ')))
 
         player_class = PlayerFactory.player_dict[selected_player]
